cfcd61ddc5d77dcd12fe8822b2c4bad920e30f12
Jakob Ackermann <das7pad@outlook.com>
2026-05-09 00:02:28 +0200

    WIP handle parallel stages -- refine spacing for collapsed

--- src/main/frontend/multi-pipeline-graph-view/multi-pipeline-graph/main/SingleRun.tsx
+++ src/main/frontend/multi-pipeline-graph-view/multi-pipeline-graph/main/SingleRun.tsx
@@ -49,14 +49,16 @@ export default function SingleRun({ run, currentJobPath }: SingleRunProps) {
       layout.nodeSpacingH = 90;
     }
 
-    layout.ypStart = layout.nodeRadius;
-    layout.nodeSpacingV = layout.nodeRadius;
-    if (showNames) {
-      layout.ypStart += 7 + layout.labelOffsetV;
+    if (!showNames) {
+      // Do not reserve space for big label.
+      layout.ypStart -= layout.labelOffsetV;
     }
-    if (showDurations) {
-      layout.nodeSpacingV += 7 + layout.smallLabelOffsetV;
+    if (!showDurations) {
+      // Do not reserve space for small label.
+      layout.nodeSpacingV -= layout.labelOffsetV;
     }
+    // Do not reserve space for big label on next row.
+    layout.nodeSpacingV -= layout.labelOffsetV;
 
     return layout;
   }
